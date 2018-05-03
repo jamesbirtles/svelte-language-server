0928b574689446e2242039ee34e74d3cb5964f5f
James Birtles <jameshbirtles@gmail.com>
2018-05-03 19:21:54 +0100

    rewrite typescript language server interface

--- src/plugins/TypeScriptPlugin.ts
+++ src/plugins/TypeScriptPlugin.ts
@@ -1,5 +1,5 @@
 import ts from 'typescript';
-import { join, resolve, basename } from 'path';
+import { join, resolve, basename, dirname, extname } from 'path';
 import * as prettier from 'prettier';
 import detectIndent from 'detect-indent';
 import indentString from 'indent-string';
@@ -17,6 +17,7 @@ import {
     FormattingProvider,
     TextEdit,
 } from '../api';
+import { SvelteDocument } from '../lib/documents/SvelteDocument';
 
 export class TypeScriptPlugin implements DiagnosticsProvider, HoverProvider, FormattingProvider {
     public static matchFragment(fragment: Fragment) {
@@ -26,19 +27,22 @@ export class TypeScriptPlugin implements DiagnosticsProvider, HoverProvider, For
     private lang = getLanguageService();
 
     getDiagnostics(document: Document): Diagnostic[] {
-        const lang = this.lang.withDocument(document);
+        const lang = this.lang.updateDocument(document);
         const syntaxDiagnostics = lang.getSyntacticDiagnostics(document.getFilePath()!);
         const semanticDiagnostics = lang.getSemanticDiagnostics(document.getFilePath()!);
         return [...syntaxDiagnostics, ...semanticDiagnostics].map(diagnostic => ({
             range: convertRange(document, diagnostic),
             severity: DiagnosticSeverity.Error,
-            source: 'js',
+            source:
+                getScriptKindFromTypeAttribute(document.getAttributes().type) === ts.ScriptKind.TS
+                    ? 'ts'
+                    : 'js',
             message: ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'),
         }));
     }
 
     doHover(document: Document, position: Position): Hover | null {
-        const lang = this.lang.withDocument(document);
+        const lang = this.lang.updateDocument(document);
         const info = lang.getQuickInfoAtPosition(
             document.getFilePath()!,
             document.offsetAt(position),
@@ -61,7 +65,7 @@ export class TypeScriptPlugin implements DiagnosticsProvider, HoverProvider, For
         const config = await prettier.resolveConfig(document.getFilePath()!);
         const formattedCode = prettier.format(document.getText(), {
             ...config,
-            parser: 'typescript', // TODO: select babylon if js only
+            parser: getParserFromTypeAttribute(document.getAttributes().type), // TODO: select babylon if js only
         });
 
         let indent = detectIndent(document.getText());
@@ -75,101 +79,117 @@ export class TypeScriptPlugin implements DiagnosticsProvider, HoverProvider, For
     }
 }
 
-function generateComponentTypings(fileName: string) {
-    const compName = basename(fileName, '.html.d.ts');
-    // TODO: flesh out
-    return `
-        export interface ${compName}Data {
-            // TODO
-        }
+interface DocumentSnapshot extends ts.IScriptSnapshot {
+    version: number;
+    scriptKind: ts.ScriptKind;
+}
 
-        export default class ${compName} {
-            constructor(opts: { target: Element, data: ${compName}Data });
-        }
-    `;
+namespace DocumentSnapshot {
+    export function fromDocument(document: Document): DocumentSnapshot {
+        const text = document.getText();
+        const length = document.getTextLength();
+        return {
+            version: document.version,
+            scriptKind: getScriptKindFromTypeAttribute(document.getAttributes().type),
+            getText: (start, end) => text.substring(start, end),
+            getLength: () => length,
+            getChangeRange: () => undefined,
+        };
+    }
 }
 
 function getLanguageService() {
+    const workspacePath = '/Users/jamesb/projects/Aircast/frontend';
+    const documents = new Map<string, DocumentSnapshot>();
+
     let compilerOptions: ts.CompilerOptions = {
         allowNonTsExtensions: true,
         target: ts.ScriptTarget.Latest,
         module: ts.ModuleKind.ESNext,
         moduleResolution: ts.ModuleResolutionKind.NodeJs,
-        strict: true,
+        allowJs: true,
     };
 
-    let currentDocument: Document;
-    const host: ts.LanguageServiceHost & ts.ModuleResolutionHost = {
+    // Grab tsconfig file
+    const configFilename =
+        ts.findConfigFile(workspacePath, ts.sys.fileExists, 'tsconfig.json') ||
+        ts.findConfigFile(workspacePath, ts.sys.fileExists, 'jsconfig.json');
+    const configJson = configFilename && ts.readConfigFile(configFilename, ts.sys.readFile).config;
+    let files: string[];
+    if (configJson) {
+        const parsedConfig = ts.parseJsonConfigFileContent(
+            configJson,
+            ts.sys,
+            workspacePath,
+            compilerOptions,
+            configFilename,
+            undefined,
+            [
+                { extension: 'html', isMixedContent: true },
+                { extension: 'svelte', isMixedContent: true },
+            ],
+        );
+        files = parsedConfig.fileNames;
+        compilerOptions = { ...compilerOptions, ...parsedConfig.options };
+    }
+
+    const host: ts.LanguageServiceHost = {
         getCompilationSettings: () => compilerOptions,
-        getScriptFileNames() {
-            const filePath = currentDocument.getFilePath()!;
-            const fileInfo = ts.preProcessFile(currentDocument.getText(), true, true);
-            const scripts = [
-                filePath,
-                filePath + '.d.ts',
-                ...fileInfo.importedFiles
-                    .map(file => {
-                        return ts.resolveModuleName(file.fileName, filePath, compilerOptions, this)
-                            .resolvedModule;
-                    })
-                    .filter(mod => !!mod)
-                    .map(mod => mod!.resolvedFileName),
-            ];
-            return scripts;
+        getScriptFileNames: () => Array.from(new Set([...files, ...Array.from(documents.keys())])),
+        getScriptVersion(fileName: string) {
+            const doc = documents.get(fileName);
+            return doc ? String(doc.version) : '0';
         },
-        getScriptVersion: (fileName: string) => {
-            if (fileName === currentDocument.getFilePath()) {
-                return String(currentDocument.version);
-            }
-            return '1';
-        },
-        getScriptSnapshot: (fileName: string) => {
-            let text = '';
-            if (fileName === currentDocument.getFilePath()) {
-                text = currentDocument.getText();
-            } else if (fileName.endsWith('.html.d.ts') && !ts.sys.fileExists(fileName)) {
-                text = generateComponentTypings(fileName);
-            } else {
-                text = ts.sys.readFile(fileName) || '';
+        getScriptSnapshot(fileName: string): ts.IScriptSnapshot | undefined {
+            const doc = documents.get(fileName);
+            if (doc) {
+                return doc;
             }
 
-            return ts.ScriptSnapshot.fromString(text);
+            return ts.ScriptSnapshot.fromString(ts.sys.readFile(fileName) || '');
         },
-        getScriptKind(fileName: string) {
-            if (fileName === currentDocument.getFilePath()) {
-                const type = currentDocument.getAttributes().type;
-                switch (type) {
-                    case 'text/typescript':
-                        return ts.ScriptKind.TS;
-                    case 'text/javascript':
-                    default:
-                        return ts.ScriptKind.JS;
+        getCurrentDirectory: () => '/Users/jamesb/projects/Aircast/frontend',
+        getDefaultLibFileName: ts.getDefaultLibFilePath,
+
+        resolveModuleNames(moduleNames: string[], containingFile: string): ts.ResolvedModule[] {
+            return moduleNames.map(name => {
+                const resolved = ts.resolveModuleName(
+                    name,
+                    containingFile,
+                    compilerOptions,
+                    ts.sys,
+                );
+
+                if (!resolved.resolvedModule && isSvelte(name)) {
+                    return {
+                        resolvedFileName: resolve(dirname(containingFile), name),
+                        extension: extname(name),
+                    };
                 }
-            }
 
-            return getScriptKindFromFileName(fileName);
-        },
-        getCurrentDirectory: () => '',
-        getDefaultLibFileName: options => ts.getDefaultLibFilePath(options),
-        fileExists(fileName: string) {
-            return (
-                ts.sys.fileExists(fileName) ||
-                (fileName.endsWith('.html.d.ts') &&
-                    ts.sys.fileExists(fileName.slice(0, fileName.length - '.d.ts'.length)))
-            );
-        },
-        readFile(fileName: string) {
-            return ts.sys.readFile(fileName);
+                return resolved.resolvedModule!;
+            });
         },
     };
-    const lang = ts.createLanguageService(host);
+    let languageService = ts.createLanguageService(host);
 
     return {
-        withDocument(document: Document) {
-            currentDocument = document;
-            return lang;
-        },
+        getService: () => languageService,
+        updateDocument,
     };
+
+    function updateDocument(document: Document): ts.LanguageService {
+        const preSnapshot = documents.get(document.getFilePath()!);
+        const newSnapshot = DocumentSnapshot.fromDocument(document);
+        if (preSnapshot && preSnapshot.scriptKind !== newSnapshot.scriptKind) {
+            // Restart language service as it doesn't handle script kind changes.
+            languageService.dispose();
+            languageService = ts.createLanguageService(host);
+        }
+
+        documents.set(document.getFilePath()!, newSnapshot);
+        return languageService;
+    }
 }
 
 function getScriptKindFromFileName(fileName: string): ts.ScriptKind {
@@ -190,9 +210,33 @@ function getScriptKindFromFileName(fileName: string): ts.ScriptKind {
     }
 }
 
+function getScriptKindFromTypeAttribute(type: string): ts.ScriptKind {
+    switch (type) {
+        case 'text/typescript':
+            return ts.ScriptKind.TS;
+        case 'text/javascript':
+        default:
+            return ts.ScriptKind.JS;
+    }
+}
+
+function getParserFromTypeAttribute(type: string): prettier.BuiltInParserName {
+    switch (type) {
+        case 'text/typescript':
+            return 'typescript';
+        case 'text/javascript':
+        default:
+            return 'babylon';
+    }
+}
+
 function convertRange(document: Document, range: { start?: number; length?: number }) {
     return Range.create(
         document.positionAt(range.start || 0),
         document.positionAt((range.start || 0) + (range.length || 0)),
     );
+}
+
+function isSvelte(filePath: string) {
+    return filePath.endsWith('.html') || filePath.endsWith('.svelte');
 }
